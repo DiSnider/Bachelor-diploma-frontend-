fe54e86cd30d0b3da38c6105ab721012dd41d4f8
Dmytro Shnaider <di.snider81@gmail.com>
2018-05-12 22:40:32 +0300

    Kiev map added

---
+++ src/components/App/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    <iframe width="700" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=%D0%9A%D0%B8%D1%97%D0%B2&key=AIzaSyD-D-zOQwtx1-ccQlkBnZ5seI-q9AtNrTQ"></iframe>
+  </div>
+</template>
+
+<script src="./App.js"></script>
+<style src="./App.css"></style>
