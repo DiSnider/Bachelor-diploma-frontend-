5b8c7ac2db43df46daed092d125f05646e53a18f
Dmytro Shnaider <di.snider81@gmail.com>
2018-05-13 00:55:54 +0300

    markers adding, dragging and removing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,59 @@
+Array.prototype.removeIf = function(callback) {
+    var i = this.length;
+    while (i--) {
+        if (callback(this[i], i)) {
+            this.splice(i, 1);
+        }
+    }
+};
+
 export default {
     name: 'app',
     data () {
       return {
-        msg: 'Huihuihui'
+        labelIndex: 0, 
+        technicalObjects: []
       }
+    },
+    methods: {
+        initMap() {
+            var myLatLng = {lat: 50.45466, lng: 30.5238};
+          
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 11,
+              center: myLatLng
+            });
+
+            var self = this;
+          
+            // This event listener calls addMarker() when the map is clicked.
+            google.maps.event.addListener(map, 'click', function(event) {
+              self.addMarker(event.latLng, map);
+            });
+        },
+        addMarker(location, map) {
+            var label = (++this.labelIndex).toString();
+            
+            var marker = new google.maps.Marker({
+              position: location,
+              label: label,
+              draggable: true,
+              map: map
+            });
+
+            this.technicalObjects.push({
+                location,
+                label
+            });
+
+            var self = this;
+            google.maps.event.addListener(marker, 'rightclick', function(event) {
+                marker.setMap(null);
+                self.technicalObjects.removeIf((item, index) => item.label == marker.label);
+            });
+        }
+    },
+    mounted() {
+        this.initMap();
     }
 }
--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <iframe width="700" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=%D0%9A%D0%B8%D1%97%D0%B2&key=AIzaSyD-D-zOQwtx1-ccQlkBnZ5seI-q9AtNrTQ"></iframe>
+    <!-- <iframe width="700" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=%D0%9A%D0%B8%D1%97%D0%B2&key=AIzaSyD-D-zOQwtx1-ccQlkBnZ5seI-q9AtNrTQ"></iframe> -->
+    <div id="map"></div>
   </div>
 </template>
 
